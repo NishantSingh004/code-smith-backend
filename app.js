0bbabfed121c390f4ece0107e206e5fdedfedc55
Nishant Singh <nishantpratapsingh333@gmail.com>
2024-12-28 15:26:44 +0530

    updated code smit backend

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ const userRoute = require('./routes/user');
 const compilerRoute = require('./routes/compiler');
 
 
-app.use(cors({
-origin: 'http://localhost:4200'  // Replace with your frontend URL
-}));
+app.use(cors())
 app.use(express.json())
 
 app.use('/users', userRoute)
@@ -28,16 +26,12 @@ reqObj['clientSecret'] = process.env.CLIENT_SECRET;
 
 axios.post('https://api.jdoodle.com/v1/execute', reqObj).then((resp)=>{
   res.json({error: false, response: resp.data})
-}).catch((err)=>{
-  console.log(err)
 })
 })
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
   console.log('connected to db');
-}).catch((err)=>{
-  console.log(err);
-});
+})
 
 app.listen(process.env.PORT, ()=> {
   console.log("server is working")
