6ef06964b69f4b8a7981a03c04ffbfe12d745560
Nishant Singh <nishantpratapsingh333@gmail.com>
2024-12-30 11:49:43 +0530

    updated app.js

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ const compilerRoute = require('./routes/compiler');
 
 const corsOptions = {
   origin: ['https://codesmith-online.netlify.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],   
+  credentials: true,                                
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
+
 
 app.use(express.json())
 
